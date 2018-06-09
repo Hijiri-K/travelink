5113b8f9078bd66670ef72444ac3ef63bd546257
Hijiri K <hijiri.k.emk.p@gmail.com>
2018-06-09 18:17:59 +0900

    chenge app.vue to planning.vue, add planning page and top page

--- app/javascript/packs/planning.js
+++ app/javascript/packs/planning.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import App from '../components/app.vue'
+import TlPlanning from '../components/planning.vue'
 
 import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en'
 Vue.use(ElementUI, { locale })
 
   new Vue({
-    el: 'app',
-    components: { App }
+    el: 'tl-planning',
+    components: { TlPlanning }
  })
